ebbde18d6ff87178ad71e8e93ae9cbd140c3d1bb
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-10-09 15:54:38 +0100

    Updates

---
+++ precache-manifest.758d9adca7f8827213671407d3b0f7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32212778d6e0808d81b26467a5eb1670",
+    "url": "/redux-shop-list/index.html"
+  },
+  {
+    "revision": "f98cfe7901b4070c1295",
+    "url": "/redux-shop-list/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "94b83cd593559f667383",
+    "url": "/redux-shop-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f98cfe7901b4070c1295",
+    "url": "/redux-shop-list/static/js/2.074f22b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/redux-shop-list/static/js/2.074f22b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b83cd593559f667383",
+    "url": "/redux-shop-list/static/js/main.d1f398ed.chunk.js"
+  },
+  {
+    "revision": "763fc85bd4039841f0fc",
+    "url": "/redux-shop-list/static/js/runtime-main.f6cd984b.js"
+  }
+]);
